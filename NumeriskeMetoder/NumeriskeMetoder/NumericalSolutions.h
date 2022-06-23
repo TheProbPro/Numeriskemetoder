30bdea8b087b2942a67800a7f825186db7db52f1
Victor Brønsholm Nielsen <Vinie20@student.sdu.dk>
2022-06-24 00:06:22 +0200

    11
    Igang med methoderne til lektion 10

--- NumeriskeMetoder/NumeriskeMetoder/NumericalSolutions.h
+++ NumeriskeMetoder/NumeriskeMetoder/NumericalSolutions.h
@@ -2,11 +2,169 @@
 #define NUMERICALSOLUTIONS_H
 
 // Includes
+#include <iostream>
+#include <cmath>
+#include "../../NR_C301/code/nr3.h"
 
 
 namespace NumericalSolutions
 {
+	//Print method
+	void printoutTable(VecDoub& a, VecDoub& fcalc) {
 
+		std::cout << std::left;
+		std::cout << std::fixed;
+		std::cout << std::setprecision(7);
+
+		std::cout << std::setw(8) << "i" << " | " << std::setw(8) << "a[i]" << " | " <<
+			std::setw(8) << "a[i-1] - a[i]" << " | " <<
+			std::setw(8) << "Rich-alp^k" << " | " <<
+			std::setw(8) << "Rich-fejl" << " | " <<
+			std::setw(8) << "Antal f-beregninger" << std::endl << std::endl;
+
+		for (size_t i = 0; i < a.size(); i++) {
+			if (i == 0) {
+				std::cout << std::setw(8) << i << " | " <<
+					std::setw(8) << a[i] << " | " <<
+					std::setw(8) << " ******* " << " | " <<
+					std::setw(8) << " ******* " << " | " <<
+					std::setw(8) << " ******* " << " | " <<
+					std::setw(8) << fcalc[i] << std::endl;
+			}
+			else if (i == 1) {
+				std::cout << std::setw(8) << i << " | " <<
+					std::setw(8) << a[i] << " | " <<
+					std::setw(8) << a[i] - a[i - 1] << "| " <<
+					std::setw(8) << " ******* " << " | " <<
+					std::setw(8) << " ******* " << " | " <<
+					std::setw(8) << fcalc[i] << std::endl;
+			}
+			else {
+				std::cout << std::setw(8) << i << " | " <<
+					std::setw(8) << a[i] << " | " <<
+					std::setw(8) << a[i] - a[i - 1] << "  |  " <<
+					std::setw(8) << abs(a[i - 2] - a[i - 1]) / (abs(a[i - 1] - a[i])) << " | " <<
+					std::setw(8) << abs((a[i] - a[i - 1]) / ((abs(a[i - 2] - a[i - 1]) / (abs(a[i - 1] - a[i])) - 1))) << " | " <<
+					std::setw(8) << fcalc[i] << std::endl << std::endl;
+			}
+		}
+
+	}
+	
+	// Eulers method
+	VecDoub Euler(VecDoub (*f)(double X0, double Y0), double X0, double Y0, int a, int b, VecDoub& h, VecDoub& ans) {
+		VecDoub Fxy(2);
+		VecDoub FxyNew(2);
+		VecDoub xy(2);
+		double stepsize = 0;
+
+		
+		for (int i = 0; i < ans.size(); ++i) {
+			xy[0] = X0;
+			xy[1] = Y0;
+			stepsize = (double(b) - double(a)) / double(h[i]);
+			for (double j = a; j < b; j = j + stepsize) {
+				Fxy = f(xy[0], xy[1]);
+				FxyNew[0] = xy[0] + stepsize * Fxy[0];
+				FxyNew[1] = xy[1] + stepsize * Fxy[1];
+				xy[0] = FxyNew[0];
+				xy[1] = FxyNew[1];
+				ans[i] = xy[0];
+			}
+		}
+		return ans;
+	}
+	
+	//--------------------------------Ikke færdige--------------------------------------------
+	// Leap Frog Method
+	VecDoub LeapFrog(VecDoub(*f)(double X0, double Y0), double X0, double Y0, int a, int b, VecDoub& h, VecDoub& ans) {
+		VecDoub Fxy(2);
+		VecDoub FxyNew(2);
+		VecDoub FxyOld(2);
+		VecDoub xy(2);
+		double stepsize = 0;
+
+		for (int i = 0; i < ans.size(); ++i) {
+			xy[0] = X0;
+			xy[1] = Y0;
+			stepsize = (double(b) - double(a)) / double(h[i]);
+			Fxy = f(xy[0], xy[1]);
+			FxyOld[0] = xy[0] - stepsize * Fxy[0];
+			FxyOld[1] = xy[1] - stepsize * Fxy[1];
+			for (double j = a; j < b; j = j + stepsize) {
+				Fxy = f(xy[0], xy[1]);
+				FxyNew[0] = FxyOld[0] + 2 * stepsize * Fxy[0];
+				FxyNew[1] = FxyOld[1] + 2 * stepsize * Fxy[1];
+				FxyOld[0] = xy[0];
+				FxyOld[1] = xy[1];
+				xy[0] = FxyNew[0];
+				xy[1] = FxyNew[1];
+				ans[i] = xy[0];
+			}
+		}
+		return ans;
+	}
+
+	
+	// Midpoint method
+	VecDoub Midpoint(VecDoub(*f)(double X0, double Y0), double X0, double Y0, int a, int b, VecDoub h, VecDoub& ans) {
+		VecDoub Fxy0(2);
+		VecDoub Fxy1(2);
+		VecDoub FxyNew(2);
+		VecDoub FxyHalf(2);
+		VecDoub xy(2);
+		double stepsize = 0;
+		
+		for (int i = 0; i < ans.size(); ++i) {
+			xy[0] = X0;
+			xy[1] = Y0;
+			stepsize = (double(b) - double(a)) / double(h[i]);
+			for (double j = a; j < b; j = j + stepsize) {
+				Fxy0 = f(xy[0], xy[1]);
+				Fxy1 = f(FxyHalf[0], FxyHalf[1]);
+				FxyHalf[0] = xy[0] + 0.5 * stepsize * Fxy0[0];
+				FxyHalf[1] = xy[1] + 0.5 * stepsize * Fxy0[1];
+				FxyNew[0] = xy[0] + stepsize * Fxy1[0];
+				FxyNew[1] = xy[1] + stepsize * Fxy1[1];
+				xy[0] = FxyNew[0];
+				xy[1] = FxyNew[1];
+				ans[i] = xy[0];
+			}
+		}
+
+		return ans;
+	}
+
+	// Trapezoidal method
+	VecDoub Trapezoidal(VecDoub(*f)(double X0, double Y0), double X0, double Y0, int a, int b, VecDoub h, VecDoub& ans) {
+		VecDoub Fxy(2);
+		VecDoub FxyEuler(2);
+		VecDoub FxyNew(2);
+		VecDoub xy(2);
+		double stepsize = 0;
+
+		for (int i = 0; i < ans.size(); ++i) {
+			xy[0] = X0;
+			xy[1] = Y0;
+			stepsize = (double(b) - double(a)) / double(h[i]);
+			for (double j = a; j < b; j = j + stepsize) {
+				Fxy = f(xy[0], xy[1]);
+				
+				
+				xy[0] = FxyNew[0];
+				xy[1] = FxyNew[1];
+				ans[i] = xy[0];
+			}
+		}
+
+		return ans;
+	}
+
+	// 4. order Runge Kutta method
+	VecDoub RungeKutta(VecDoub(*f)(double X0, double Y0), double X0, double Y0, int a, int b, VecDoub h, VecDoub& ans) {
+		return ans;
+	}
+	
 };
 
 #endif // NUMERICALSOLUTIONS_H
--- NumeriskeMetoder/NumeriskeMetoder/NumeriskeMetoder.cpp
+++ NumeriskeMetoder/NumeriskeMetoder/NumeriskeMetoder.cpp
@@ -442,9 +442,107 @@ int main() {
 */
 
 //Lektion 10
+#include "NumericalSolutions.h"
+
+double Leap_Frog(double x0, double y0, int a, int b, int hinv)
+{
+	double x = x0;
+	double y = y0;
+
+	double t = a;
+	double stepsize = (double(b) - double(a)) / double(hinv);
+
+	double xminus1 = x - stepsize * (x * y);		// Using Euler
+	double yminus1 = y - stepsize * (-pow(x, 2));	// Using Euler
+
+	//std::cout << "Leap_Frog method, with stepsize " << stepsize << std::endl;
+	//std::cout << "x(0) = " << x << std::endl;
+	//std::cout << "y(0) = " << y << std::endl;
+	//std::cout << "x^2 + y^2 = " << pow(x,2)+pow(y,2) << std::endl;
+	//std::cout << std::endl;
+
+	for (double i = a; i < b; i = i + stepsize)
+	{
+		double xplus1 = xminus1 + 2 * stepsize * (x * y);
+		double yplus1 = yminus1 + 2 * stepsize * (-pow(x, 2));
+		xminus1 = x;
+		yminus1 = y;
+		x = xplus1;
+		y = yplus1;
+		t = t + stepsize;
+		//	std::cout << "x(" << t << ") = " << x << std::endl;
+		//	std::cout << "y(" << t << ") = " << y << std::endl;
+		//	std::cout << "x^2 + y^2 = " << pow(x,2)+pow(y,2) << std::endl;
+		//	std::cout << std::endl;
+
+	}
+	return(x);
+
+}
+
+VecDoub f(double X0, double Y0) {
+	VecDoub f(2);
+	f[0] = X0 * Y0;
+	f[1] = (-pow(X0, 2));
+	return f;
+}
 
 int main() {
-	
+	VecDoub eul(10);
+	VecDoub LF(10);
+	VecDoub Mp(10);
+	VecDoub h(10);
+	for (int i = 0; i < eul.size(); ++i) {
+		h[i] = 5 * pow(2, i);
+	}
+	std::cout << "Eulers method: " << std::endl;
+	NumericalSolutions::Euler(f, 1, 1, 0, 5, h, eul);
+	NumericalSolutions::printoutTable(eul, h);
+
+	std::cout << "Leap Frog method: " << std::endl;
+	NumericalSolutions::LeapFrog(f, 1, 1, 0, 5, h, LF);
+	NumericalSolutions::printoutTable(LF, h);
+
+	std::cout << "Midpoint method: " << std::endl;
+	NumericalSolutions::Midpoint(f, 1, 1, 0, 5, h, Mp);
+	NumericalSolutions::printoutTable(Mp, h);
+
+
+
+	Doub s, olds = 0.0, oldolds = 0.0, rn = 0.0, r = 0.0, oldr = 0.0, oldoldr = 0.0;
+	cout << setw(15) << "hinv" << setw(15) << "Sn" << setw(15) << "k1" << setw(15) << "Rn" << setw(15) << "k2" << setw(15) << "R2n" << endl;
+	for (int j = 0; j < 15; j++) {
+
+
+		//s = Euler(1,1,0,5,5*pow(2,j));  // 1(1) and 2(3)
+		s = Leap_Frog(1, 1, 0, 10, 5 * pow(2, j));//1(1) and 2(3)
+
+		//s =Midpoint(1,1,0,5,5*pow(2,j)); // 2(3) and 3(8)
+
+		//s=Trapezoidal(1,1,0,5,5*pow(2,j));// 2(3) and 3(8)
+
+		//s = CalcRK4(1,1,0,5,5*pow(2,j)); //4(15) and 5(24)
+
+		cout << setw(15) << 5 * pow(2, j);
+		cout << setw(15) << s;
+		if (oldolds != 0.0) { cout << setw(15) << log2(abs((oldolds - olds) / (olds - s))); }
+		else { cout << setw(15) << " "; }
+		if (olds != 0.0) { r = s + (s - olds) / 1.0; cout << setw(15) << r; } // We see that k1 tends towards 2,and with alpha=2 we get alpha^k-1 = 3
+		else { cout << setw(15) << " "; }
+		if (oldoldr != 0.0) { cout << setw(15) << log2(abs((oldoldr - oldr) / (oldr - r))); }
+		else { cout << setw(15) << " "; }
+		if (oldr != 0.0) { rn = r + (r - oldr) / 3.0; cout << setw(15) << rn; } // We see that k2 tends towards 4,and with alpha=2 we get alpha^k-1 = 15
+		else { cout << setw(15) << " "; }
+		cout << endl;
+
+
+		oldoldr = oldr;
+		oldolds = olds;
+		oldr = r;
+		olds = s;
+
+
+	}
 
 	return 0;
 }
@@ -530,3 +628,4 @@ int main() {
 
 	return 0;
 }*/
+
